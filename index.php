972e5c2763d17835ec8a607d9a67754c6d9ecfdf
RANAIVOARISON Mionja <mionjaran2005@gmail.com>
2022-07-28 18:09:27 -0600

    just a test

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
      <?php
         include "Views/shared/header.php";
 
-        require('Controllers/Ctrl_pages.php');
-        Ctrl_pages::index();
+        $auth = isset($_GET['auth']) ? $_GET['auth'] : NULL;
+        
+        if (isset($auth)) {
+            require('Controllers/Ctrl_auths.php');
+            Ctrl_auths::index();
+        }
+        else{
+            require('Controllers/Ctrl_pages.php');
+            Ctrl_pages::index();
+        }
         
         include "Views/shared/footer.php";
     ?>
